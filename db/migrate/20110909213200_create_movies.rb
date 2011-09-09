97bf97f3a081728cc6f28dd8c016e1b2a2e68e2d
Armando Fox <fox@cs.berkeley.edu>
2011-09-09 15:51:39 -0700

    Continuing cleanup for initial exposition of app in Arch chapter

--- db/migrate/20110909213200_create_movies.rb
+++ db/migrate/20110909213200_create_movies.rb
@@ -3,6 +3,7 @@ class CreateMovies < ActiveRecord::Migration
     create_table :movies do |t|
       t.string :title
       t.string :rating
+      t.text :description
       t.datetime :released_on
 
       t.timestamps
