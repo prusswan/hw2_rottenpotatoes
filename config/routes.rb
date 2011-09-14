b159472740def9dbb1de3249d51d45a048ffc0fb
Armando Fox <fox@cs.berkeley.edu>
2011-09-13 19:45:06 -0700

    need default route to match non-RESTful controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rottenpotatoes::Application.routes.draw do
   # The root of the site ('/') is an alias for 'list all movies'.
   root :to => 'movies#index'
 
+  # Otherwise, assume the route names the controller and action
+  match ':controller/:action'
+
   # See how all your routes lay out with "rake routes"
 end
