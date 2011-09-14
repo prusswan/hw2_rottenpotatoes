23f9f2a5a5996006c60367fb143b77718dec8d34
Armando Fox <fox@cs.berkeley.edu>
2011-09-14 10:05:18 -0700

    Files reflect initial 'going from red to green' section of testing chapter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
   end
 
   def search_tmdb
+    @movies = Movie.find_in_tmdb(params[:search_terms])
   end
 
   def new
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe MoviesController do
   describe 'searching TMDb' do
     it 'should call the model method that performs TMDb search' do
-      post :search_tmdb, :params => {:search_terms => 'hardware'}
+      Movie.should_receive(:find_in_tmdb).with('hardware')
+      post :search_tmdb, {:search_terms => 'hardware'}
     end
     it 'should select the Search Results template for rendering'
     it 'should make the TMDb search results available to that template'
