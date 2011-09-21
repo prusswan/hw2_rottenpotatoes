041c58d43aa416988af15dce232b5b03eb08e2a8
Armando Fox <fox@cs.berkeley.edu>
2011-09-21 09:34:17 -0700

    passing simple controller spec for tdd chapter

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,11 +2,20 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'searching TMDb' do
+    before :each do
+      Movie.stub(:find_in_tmdb).and_return('matching movies')
+    end      
     it 'should call the model method that performs TMDb search' do
       Movie.should_receive(:find_in_tmdb).with('hardware')
       post :search_tmdb, {:search_terms => 'hardware'}
     end
-    it 'should select the Search Results template for rendering'
-    it 'should make the TMDb search results available to that template'
+    it 'should select the Search Results template for rendering' do
+      post :search_tmdb, {:search_terms => 'hardware'}
+      response.should render_template('search_tmdb')
+    end
+    it 'should make the TMDb search results available to that template' do
+      post :search_tmdb, :search_string => 'hardware'
+      assigns(:movies).should == 'matching movies'
+    end
   end
 end
