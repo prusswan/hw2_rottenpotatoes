1d8b4f6a09604af0eb6dbca4b8422e4ebb9e88fe
Armando Fox <fox@cs.berkeley.edu>
2011-10-01 13:10:36 -0700

    complete controller spec for search_tmdb

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,21 +3,22 @@ require 'spec_helper'
 describe MoviesController do
   describe 'searching TMDb' do
     before :each do
-      Movie.stub(:find_in_tmdb).and_return('matching movies')
-    end      
+      @fake_results = [mock('Movie'), mock('Movie')]
+    end
     it 'should call the model method that performs TMDb search' do
-      Movie.should_receive(:find_in_tmdb).with('hardware')
+      Movie.should_receive(:find_in_tmdb).with('hardware').
+        and_return(@fake_results)
       post :search_tmdb, {:search_terms => 'hardware'}
     end
     it 'should select the Search Results template for rendering' do
+      Movie.stub(:find_in_tmdb).and_return(@fake_results)
       post :search_tmdb, {:search_terms => 'hardware'}
       response.should render_template('search_tmdb')
     end
     it 'should make the TMDb search results available to that template' do
-      fake_results = [mock_model('Movie'), mock_model('Movie')]
-      Movie.stub(:find_in_tmdb).and_return(fake_results)
-      post :search_tmdb, :search_string => 'hardware'
-      assigns(:movies).should == fake_results
+      Movie.stub(:find_in_tmdb).and_return(@fake_results)
+      post :search_tmdb, {:search_terms => 'hardware'}
+      assigns(:movies).should == @fake_results
     end
   end
 end
