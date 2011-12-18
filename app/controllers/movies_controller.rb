d2eebe59c72338fe5d430a1c12debe34040d7a0b
Armando Fox <fox@cs.berkeley.edu>
2011-12-18 09:51:22 -0800

    minor changes to make consistent with ruby intro chapter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,4 +31,11 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
 end
