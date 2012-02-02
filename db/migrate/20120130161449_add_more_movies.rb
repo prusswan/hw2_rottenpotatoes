ec26914342c7c74cb3e9cc9e2c65fd49229747e5
Sam Whitlock <phynominal@gmail.com>
2012-02-01 23:09:04 -0800

    minor migration fix

--- db/migrate/20120130161449_add_more_movies.rb
+++ db/migrate/20120130161449_add_more_movies.rb
@@ -5,7 +5,7 @@ class AddMoreMovies < ActiveRecord::Migration
     {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
     {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
     {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-    {:title => 'Amelie', :rating => 'R', :release_date => '52-Apr-2001'},
+    {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
     {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
     {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
     {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
